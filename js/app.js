e2069af733487f56e6d85dc8e43f1cd9e6c16815
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2018-08-10 22:39:07 -0500

    basic movement

--- js/app.js
+++ js/app.js
@@ -6,8 +6,42 @@ let windowedHeight = 640;
 canvas.width = windowedWidth;
 canvas.height = windowedHeight;
 
+let forkLift = {
+  x: 200,
+  y: 200,
+  speed: 2,
+}
+
 function update() {
+  canvas.width = canvas.width;
+  ctx.fillRect(forkLift.x,forkLift.y,20,20);
   requestAnimationFrame(update);
+}
+
+window.addEventListener("keydown", onKeyDown, false);
+window.addEventListener("keyup", onKeyUp, false);
+
+function onKeyDown(event){
+  var keyCode = event.keyCode;
+  if(keyCode == 37) {
+    forkLift.x-= forkLift.speed;
+  }
+  if(keyCode == 38) {
+    forkLift.y-= forkLift.speed;
+  }
+  if(keyCode == 39) {
+    forkLift.x+= forkLift.speed;
+  }
+  if(keyCode == 40) {
+    forkLift.y+= forkLift.speed;
+  }
+}
+
+function onKeyUp(event){
+  var keyCode = event.keyCode;
+  if(keyCode == 32) {
+
+  }
 }
 
 window.addEventListener("load", function() {
