2ceb1805d5b42cdb7831c55d52d4d46b0fdc5c1b
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2018-08-10 23:17:40 -0500

    updated player movement

--- js/app.js
+++ js/app.js
@@ -9,13 +9,52 @@ canvas.height = windowedHeight;
 let forkLift = {
   x: 200,
   y: 200,
-  speed: 2,
+  speed: 1,
 }
+
+let pressedKeys = [];
 
 function update() {
   canvas.width = canvas.width;
   ctx.fillRect(forkLift.x,forkLift.y,20,20);
+  playerMovement(forkLift);
   requestAnimationFrame(update);
+}
+
+function playerMovement(player) {
+  let LEFT = pressedKeys.includes(37);
+  let UP = pressedKeys.includes(38);
+  let RIGHT = pressedKeys.includes(39);
+  let DOWN = pressedKeys.includes(40);
+  let rootSpd = Math.sqrt(player.speed)
+  if (UP && !LEFT && !RIGHT) {
+    player.y -=player.speed;
+  }
+  if (UP && LEFT) {
+    player.y -= rootSpd;
+    player.x -= rootSpd;
+  }
+  if (UP && RIGHT) {
+    player.y -= rootSpd;
+    player.x += rootSpd;
+  }
+  if (DOWN && !LEFT && !RIGHT) {
+    player.y +=player.speed;
+  }
+  if (DOWN && LEFT) {
+    player.y += rootSpd;
+    player.x -= rootSpd;
+  }
+  if (DOWN && RIGHT) {
+    player.y += rootSpd;
+    player.x += rootSpd;
+  }
+  if (LEFT && !UP && !DOWN) {
+    player.x -=player.speed;
+  }
+  if (RIGHT && !UP && !DOWN) {
+    player.x +=player.speed;
+  }
 }
 
 window.addEventListener("keydown", onKeyDown, false);
@@ -23,26 +62,18 @@ window.addEventListener("keyup", onKeyUp, false);
 
 function onKeyDown(event){
   var keyCode = event.keyCode;
-  if(keyCode == 37) {
-    forkLift.x-= forkLift.speed;
-  }
-  if(keyCode == 38) {
-    forkLift.y-= forkLift.speed;
-  }
-  if(keyCode == 39) {
-    forkLift.x+= forkLift.speed;
-  }
-  if(keyCode == 40) {
-    forkLift.y+= forkLift.speed;
+  if (!pressedKeys.includes(keyCode)) {
+    pressedKeys.push(keyCode);
   }
 }
 
 function onKeyUp(event){
   var keyCode = event.keyCode;
-  if(keyCode == 32) {
-
+  if (pressedKeys.includes(keyCode)) {
+    pressedKeys.splice(pressedKeys.indexOf(keyCode),1);
   }
 }
+
 
 window.addEventListener("load", function() {
   update();
